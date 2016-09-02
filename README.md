0f17cd287f5994b413632d323eec26629657b4b7
Andres Garcia <andresgarciar96@gmai.com>
2016-09-02 18:36:25 -0400

    finally get the list to show orders

---
+++ README.md
@@ -0,0 +1,46 @@
+# _Ping pong Webpage_
+
+
+#### By _**Andres Garcia**_
+
+## Description
+
+_This webpage will let you play ping pong!!_
+
+## Support and contact details
+
+_Feel free to contact me at andresgarciar96@gmail.com_
+
+## Technologies Used
+
+_Javascript, HTML, Atom._
+## Installation Requirements
+
+_Download or clone from git, open in atom and mess around with the code_
+## Specifications
+
+_This page will accept the users number_
+* Input--> 12
+
+_This page will display the number count from the number input_
+* Input: 12.
+* Output: 1,2,3,4,5,6,7,8,9,10,11,12.
+
+_This webpage will turn the numbers divisible by 3 to pings_
+* Input: 10.
+* Output: 1,2,ping,5,ping,7,8,ping,10.
+
+_This webpage will turn the numbers divisible by 5 to pongs_
+* Input: 10.
+* Output: 1,2,3,4,pong,6,7,8,9,pong.
+
+_This webpage will turn numbers divisible by both 5 and 3 to ping pongs_
+* Input: 15.
+* Output: 1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, Ping Pong.
+
+_This web page will display the numbers of pings, pongs, and ping pongs_
+* Input: 15.
+* Output: Pings: 5, Pongs : 3, Ping Pongs : 1.
+
+
+Copyright (c) 2016 **_Andres Garcia_**
---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+//Bussines Logic
+
+var Customer = function( first, last, address)
+{
+  this.firstName = first;
+  this.lastName = last;
+  this.addres = address;
+}
+var Pizza = function( orderStyle, orderSize, order)
+{
+  this.orderStyle = orderStyle;
+  this.orderSize = orderSize;
+  this.order = order;
+  this.price = 0;
+  this.toppings = [];
+}
+Customer.prototype.information = function() {
+  return this.lastName + ", " + this.firstName;
+}
+
+Pizza.prototype.fullPrice = function () {
+  this.price += (this.toppings.length * 2)
+
+  if (this.orderStyle === "delivery") {
+    this.price += 5;
+  }
+
+  if (this.orderSize === "small") {
+    this.price += 3;
+  }
+  else if (this.orderSize === "medium") {
+    this.price += 5;
+  }
+  else if (this.orderSize === "large") {
+    this.price += 8;
+  }
+  else if (this.orderSize === "extra-large") {
+    this.price += 11;
+  }
+}
+
+//User Interface
+$(document).ready(function() {
+var orderNumber = 0;
+  $("form#user-info").submit(function(event) {
+    event.preventDefault();
+    orderNumber++;
+
+
+    var firstName = $("#first-name").val();
+    var lastName = $("#last-name").val();
+    var address = $("#address").val();
+
+    var newCustomer  =  new Customer(firstName, lastName, address);
+
+    var orderStyleInput = $("input:radio[name='order-style']:checked").val();
+    var orderSizeInput = $("input:radio[name='size']:checked").val();
+
+
+    var newPizza = new Pizza(orderStyleInput, orderSizeInput, orderNumber);
+    $("input[type='checkbox']:checked").each(function(){
+      newPizza.toppings.push($(this).val());
+    })
+    newPizza.fullPrice();
+
+
+    $(".orders").append("<li>" + "order " + newPizza.order + "</li>");
+
+})
+})
